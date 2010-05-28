cab6a9087820674657aec8bb29ec86585cd95906
upsilon <kim.upsilon@gmail.com>
2010-05-29 02:47:34 +0900

    ScrappintResult.getMapメソッドを追加

--- ScrappingResult.java
+++ ScrappingResult.java
@@ -16,10 +16,24 @@ public class ScrappingResult
     return list.isEmpty() ? null : list.get(0);
   }
 
-  public ArrayList<String> getMulti(String key)
+  public List<String> getMulti(String key)
   {
     ArrayList<String> list = result.get(key);
     return list.isEmpty() ? null : list;
+  }
+
+  public Map<String, String> getMap(String key1, String key2)
+  {
+    HashMap<String, String> result = new HashMap<String, String>();
+
+    List<String> list1 = getMulti(key1);
+    List<String> list2 = getMulti(key2);
+    for (int i = 0; i < list1.size(); i++)
+    {
+      result.put(list1.get(i), list2.get(i));
+    }
+
+    return result;
   }
 
   public Iterator<Map.Entry<String, ArrayList<String>>> iterator()
