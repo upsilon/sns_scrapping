060c0b5239bfdef2800ecd3384a0b37bce839cc0
upsilon <kim.upsilon@gmail.com>
2010-06-02 00:34:19 +0900

    ScrappingResult.getMapIterableメソッドを追加

--- ScrappingResult.java
+++ ScrappingResult.java
@@ -36,8 +36,85 @@ public class ScrappingResult
     return result;
   }
 
+  public Iterable<Map<String, String>> getMapIterable(String... keys)
+  {
+    return new MapIterable(this, keys);
+  }
+
   public Iterator<Map.Entry<String, ArrayList<String>>> iterator()
   {
     return result.entrySet().iterator();
+  }
+}
+
+class MapIterable implements Iterable<Map<String, String>>
+{
+  private String[] keys;
+  private List<String>[] lists;
+
+  @SuppressWarnings({"unchecked"})
+  public MapIterable(ScrappingResult result, String... keys)
+  {
+    this.keys = keys;
+
+    lists = (List<String>[]) new List[keys.length];
+    for (int i = 0; i < keys.length; i++)
+    {
+      lists[i] = result.getMulti(keys[i]);
+    }
+  }
+
+  public Iterator<Map<String, String>> iterator()
+  {
+    return new Iter();
+  }
+
+  class Iter implements Iterator<Map<String, String>>
+  {
+    public Iterator<String>[] iters;
+
+    @SuppressWarnings({"unchecked"})
+    public Iter()
+    {
+      iters = (Iterator<String>[]) new Iterator[lists.length];
+
+      for (int i = 0; i < lists.length; i++)
+      {
+        iters[i] = lists[i].iterator();
+      }
+    }
+
+    public boolean hasNext()
+    {
+      for (Iterator<String> iter : iters)
+      {
+        if (iter.hasNext())
+        {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    public Map<String, String> next()
+    {
+      HashMap<String, String> result = new HashMap<String, String>();
+
+      for (int i = 0; i < iters.length; i++)
+      {
+        Iterator<String> iter = iters[i];
+        if (iter.hasNext())
+        {
+          result.put(keys[i], iter.next());
+        }
+      }
+
+      return result;
+    }
+
+    public void remove()
+    {
+    }
   }
 }
